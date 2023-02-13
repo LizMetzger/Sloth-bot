6f81c255e4583829fae53cbe585f6973161310b2
liz <liz.mtzgr.00@gmail.com>
2023-02-12 19:10:00 -0600

    wrote a script to measure the live position of servos, can use it to measure one at a time or all of them

---
+++ climbing/include/climbing/control/control_dyn.h
@@ -0,0 +1,14 @@
+#ifndef CONTROL_DYN_GAURD_HPP
+#define CONTROL_DYN_GAURD_HPP
+
+#include<cmath>
+#include<iostream>
+
+namespace climbing
+{
+    constexpr int deg2tics(double deg);
+
+    constexpr double tics2deg(int tics);
+}
+
+#endif
--- read_position/src/read/read_pose.cpp
+++ read_position/src/read/read_pose.cpp
@@ -1,24 +1,25 @@
+
 #include <fcntl.h>
 #include <termios.h>
 #define STDIN_FILENO 0
-
 #include <stdlib.h>
 #include <stdio.h>
-#include "control/control_dyn.h"
+#include <chrono>
+#include <thread>
 #include "dynamixel_sdk/dynamixel_sdk.h"
 #include "dynamixel_sdk/port_handler.h"
 #include "dynamixel_sdk/port_handler_linux.h"
 
-#define X_SERIES
-#define ADDR_HOMING_OFFSET          20
+#define X_SERIES // X330, X430, X540, 2X430
 #define ADDR_TORQUE_ENABLE          64
 #define ADDR_GOAL_POSITION          116
 #define ADDR_PRESENT_POSITION       132
 #define MINIMUM_POSITION_LIMIT      0  // Refer to the Minimum Position Limit of product eManual
 #define MAXIMUM_POSITION_LIMIT      4095  // Refer to the Maximum Position Limit of product eManual
 #define BAUDRATE                    57600
-
 #define PROTOCOL_VERSION  2.0
+
+#define NUMB_OF_DYNAMIXELS              4
 
 #define DEVICENAME  "/dev/ttyUSB0"
 
@@ -26,12 +27,6 @@
 #define TORQUE_DISABLE                  0
 #define DXL_MOVING_STATUS_THRESHOLD     20  // DYNAMIXEL moving status threshold
 #define ESC_ASCII_VALUE                 0x1b
-
-#define NUMB_OF_DYNAMIXELS              4
-
-// enter id number and how much you want to adjust the position by or the goal position???
-// take the id and the adjustment and move the servo to the appropriate position 
-// wait for another input
 
 int getch() {
   struct termios oldt, newt;
@@ -45,7 +40,7 @@ int getch() {
   return ch;
 }
 
-int main(){
+int main() {
   // Initialize PortHandler instance
   // Set the port path
   // Get methods and members of PortHandlerLinux or PortHandlerWindows
@@ -56,12 +51,10 @@ int main(){
   // Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
   dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
 
-  int index = 0;
   int dxl_comm_result = COMM_TX_FAIL;             // Communication result
   int dxl_goal_position[2] = {MINIMUM_POSITION_LIMIT, MAXIMUM_POSITION_LIMIT};         // Goal position
 
-  uint8_t dxl_error = 0;                          // DYNAMIXEL err
-
+  uint8_t dxl_error = 0;                          // DYNAMIXEL error
   int32_t dxl_present_position = 0;  // Read 4 byte Position data
 
   // Open port
@@ -86,57 +79,48 @@ int main(){
     return 0;
   }
 
-  // Enable DYNAMIXEL Torque
-  for (int i = 1; i <= NUMB_OF_DYNAMIXELS; i++){
-    dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, i, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
-    if (dxl_comm_result != COMM_SUCCESS) {
-        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-    }
-    else if (dxl_error != 0) {
-        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-    }
-    else {
-        printf("Succeeded enabling DYNAMIXEL Torque for servo ID %d \n", i);
-    }
-  }
-
-
   while(1) {
-    int dxl_id = -1;
-    int goal_pose = -1000;
-    while ((dxl_id <= 1 || dxl_id >= NUMB_OF_DYNAMIXELS) || (goal_pose < MINIMUM_POSITION_LIMIT || goal_pose > MAXIMUM_POSITION_LIMIT)){
-        printf("Enter a servo ID (int) and a goal position (int) (Press [ESC] to exit)\n");
-        scanf("%d %d", &dxl_id, &goal_pose);
-    }
-        
-
-    // Write goal position
-    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, dxl_id, ADDR_GOAL_POSITION, goal_pose, &dxl_error);
-    if (dxl_comm_result != COMM_SUCCESS) {
-      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-    }
-    else if (dxl_error != 0) {
-      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
-    }
+    printf("Press any key to continue. (Press [ESC] to exit)\n");
+    if (getch() == ESC_ASCII_VALUE)
+      break;
 
     do {
+      // use this block of code for only reading one at
       // Read the Present Position
-      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, dxl_id, ADDR_PRESENT_POSITION, (uint32_t*)&dxl_present_position, &dxl_error);
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, 4, ADDR_PRESENT_POSITION, (uint32_t*)&dxl_present_position, &dxl_error);
       if (dxl_comm_result != COMM_SUCCESS) {
-        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
       }
       else if (dxl_error != 0) {
-        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
       }
 
-      printf("[ID:%03d] Goal Position:%03d  Present Position:%03d\n", dxl_id, goal_pose, dxl_present_position);
+      printf("[ID:%03d] Present Position:%03d\n", 4, dxl_present_position);
+      printf("\n\n\n\n");
+      for (int j = 0; j < 1000000; j++){}
 
-    } while((abs(goal_pose - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+      // use this block of code for reading all of them
+      // for (int i = 1; i <= NUMB_OF_DYNAMIXELS; i++){
+      //       // Read the Present Position
+      //       dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, i, ADDR_PRESENT_POSITION, (uint32_t*)&dxl_present_position, &dxl_error);
+      //       if (dxl_comm_result != COMM_SUCCESS) {
+      //       printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+      //       }
+      //       else if (dxl_error != 0) {
+      //       printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+      //       }
+
+      //       printf("[ID:%03d] Present Position:%03d\n", i, dxl_present_position);
+      //       }
+      //   printf("\n\n\n\n");
+      //   for (int j = 0; j < 1000000; j++){}
+    } while(1);
+
   }
 
   // Disable DYNAMIXEL Torque
-  for (int j = 1; j <= NUMB_OF_DYNAMIXELS; j++){
-    dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, j, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  for (int i = 1; i <= NUMB_OF_DYNAMIXELS; i++){
+    dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, i, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
     if (dxl_comm_result != COMM_SUCCESS) {
         printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
     }
@@ -144,7 +128,7 @@ int main(){
         printf("%s\n", packetHandler->getRxPacketError(dxl_error));
     }
     else {
-        printf("Succeeded disabling DYNAMIXEL Torque for servo id %d.\n", j);
+        printf("Succeeded disabling DYNAMIXEL Torque.\n");
     }
   }
 
