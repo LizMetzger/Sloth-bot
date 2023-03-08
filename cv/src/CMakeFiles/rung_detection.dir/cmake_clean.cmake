fdef18a6bdc15864183757ac7f8bd4ca2737db6d
liz <liz.mtzgr.00@gmail.com>
2023-03-08 13:32:48 -0600

    made a new file to make a full auto climb for my ladder

---
+++ cv/src/CMakeFiles/rung_detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rung_detection.dir/rung_detection.cpp.o"
+  "CMakeFiles/rung_detection.dir/rung_detection.cpp.o.d"
+  "rung_detection"
+  "rung_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rung_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cv/src/rung_detection.cpp
+++ cv/src/rung_detection.cpp
@@ -2,38 +2,42 @@
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+
+#include "control/control_dyn.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+#include "dynamixel_sdk/port_handler.h"
+#include "dynamixel_sdk/port_handler_linux.h"
+
 #include <iostream>
-using namespace cv2;
-Mat src, src_gray;
-Mat dst, detected_edges;
-int lowThreshold = 0;
-const int max_lowThreshold = 100;
-const int ratio = 3;
-const int kernel_size = 3;
-const char* window_name = "Edge Map";
-static void CannyThreshold(int, void*)
-{
-    blur( src_gray, detected_edges, cv::Size(3,3) );
-    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-    dst = Scalar::all(0);
-    src.copyTo( dst, detected_edges);
-    imshow( window_name, dst );
+using namespace cv;
+
+using namespace std;
+
+int main() {
+
+Mat image;
+
+namedWindow("Display window");
+
+VideoCapture cap(0);
+
+if (!cap.isOpened()) {
+
+cout << "cannot open camera";
+
 }
-int main( int argc, char** argv )
-{
-  CommandLineParser parser( argc, argv, "{@input | fruits.jpg | input image}" );
-  src = imread( samples::findFile( parser.get<String>( "@input" ) ), IMREAD_COLOR ); // Load an image
-  if( src.empty() )
-  {
-    std::cout << "Could not open or find the image!\n" << std::endl;
-    std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
-    return -1;
-  }
-  dst.create( src.size(), src.type() );
-  cvtColor( src, src_gray, COLOR_BGR2GRAY );
-  namedWindow( window_name, WINDOW_AUTOSIZE );
-  createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-  CannyThreshold(0, 0);
-  waitKey(0);
-  return 0;
+
+while (true) {
+
+cap >> image;
+
+imshow("Display window", image);
+
+waitKey(25);
+
+}
+
+return 0;
 }
